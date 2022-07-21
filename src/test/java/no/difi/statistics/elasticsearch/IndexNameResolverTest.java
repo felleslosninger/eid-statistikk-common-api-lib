08e3a011ccacfd3b5ad8a91decf1dfad09d2a4e7
Claus Guttesen <kometen@gmail.com>
2022-07-21 12:07:47 +0200

    AOS-160 Upgraded junit-version. (#4)
    Upgraded junit-version.

--- src/test/java/no/difi/statistics/elasticsearch/IndexNameResolverTest.java
+++ src/test/java/no/difi/statistics/elasticsearch/IndexNameResolverTest.java
@@ -1,9 +1,8 @@
 package no.difi.statistics.elasticsearch;
 
 import no.difi.statistics.model.TimeRange;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.rules.ExpectedException;
+import org.hamcrest.MatcherAssert;
+import org.junit.jupiter.api.Test;
 
 import java.time.ZonedDateTime;
 import java.util.List;
@@ -13,39 +12,35 @@ import static java.time.ZonedDateTime.now;
 import static no.difi.statistics.elasticsearch.IndexNameResolver.resolveIndexName;
 import static no.difi.statistics.model.TimeSeriesDefinition.builder;
 import static org.hamcrest.Matchers.contains;
-import static org.junit.Assert.assertThat;
 
 public class IndexNameResolverTest {
-
-    @Rule
-    public ExpectedException expectedException = ExpectedException.none();
 
     @Test
     public void givenYearSeriesWhenResolvingThenResultIsOneNameWithoutDate() {
         List<String> indexNames = resolveIndexName().seriesDefinition(builder().name("test").years().owner("owner"))
                 .range(new TimeRange(now(), now())).list();
-        assertThat(indexNames, contains("owner@test@year"));
+        MatcherAssert.assertThat(indexNames, contains("owner@test@year"));
     }
 
     @Test
     public void givenMinuteSeriesWithinDayWhenResolvingThenResultIsOneNameWithYear() {
         List<String> indexNames = resolveIndexName().seriesDefinition(builder().name("test").minutes().owner("owner"))
                 .range(new TimeRange(timestamp(2016, 3, 22, 1, 23), timestamp(2016, 3, 22, 17, 18))).list();
-        assertThat(indexNames, contains("owner@test@minute2016"));
+        MatcherAssert.assertThat(indexNames, contains("owner@test@minute2016"));
     }
 
     @Test
     public void givenMonthSeriesWithinYearWhenResolvingThenResultIsOneNameWithYear() {
         List<String> indexNames = resolveIndexName().seriesDefinition(builder().name("test").months().owner("owner"))
                 .range(new TimeRange(timestamp(2016, 1, 22), timestamp(2016, 6, 30))).list();
-        assertThat(indexNames, contains("owner@test@month2016"));
+        MatcherAssert.assertThat(indexNames, contains("owner@test@month2016"));
     }
 
     @Test
     public void givenMonthSeriesCrossingYearsWhenResolvingThenResultIsOneNamePerYear() {
         List<String> indexNames = resolveIndexName().seriesDefinition(builder().name("test").months().owner("owner"))
                 .range(new TimeRange(timestamp(2014, 1, 22), timestamp(2016, 6, 30))).list();
-        assertThat(indexNames, contains("owner@test@month2014", "owner@test@month2015", "owner@test@month2016"));
+        MatcherAssert.assertThat(indexNames, contains("owner@test@month2014", "owner@test@month2015", "owner@test@month2016"));
     }
 
     private ZonedDateTime timestamp(int year, int month, int day) {
